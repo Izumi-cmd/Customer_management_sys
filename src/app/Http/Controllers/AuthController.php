ca740729732c4ea5cc2ec38753959418e6c3f9d7
Izumi_cmd <ryutaro.jp@gmail.com>
2024-08-28 01:28:43 +0900

    :fix:ルーティング修正

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -6,16 +6,29 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    /**
+     * ユーザー登録画面
+     */
     public function register(Request $request)
     {
         return view('register');
     }
 
+    /**
+     * ログイン画面
+     * @Todo ログイン処理とポリシーを作成する
+     *
+     */
     public function login(Request $request)
     {
-        return view('login');
+        return view('index');
     }
 
+    /**
+     * ログアウト処理
+     * @todo ログインユーザーのログアウト処理を作成する
+     *
+     */
     public function logout(Request $request)
     {
         return redirect()->route('index');
